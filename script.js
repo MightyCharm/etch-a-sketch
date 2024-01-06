d8b46e3190920aa99c1ecbf91c993bbc6a7ffa32
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-01-06 12:35:59 +0100

    Refactor code and add function main()

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
 function hover(e) {
-    console.log("function hover()");
+    console.log("function hover(e)");
     //console.log(e.target);
     e.target.style.backgroundColor = "black";
 
 }
-
-
-function setSquareSize(number) {
-    console.log("function setSquareSize()");
-
-    // calculate size of squares
-    // 1. get container width
-    let sizeContainer = document.querySelector("#container").clientWidth;
-    console.group(sizeContainer);
-    // 2.calculate square width/height
-    let size = (sizeContainer / number);
-    size = size +"px";
-    console.log(size)
-    return size;
-
-}
-
-
-function setGrid(number) {
-    console.log("function setGrid()");
-    // check if a grid is there, if so delete before new created
-    if (container.lastElementChild != null) {
-        let child = container.lastElementChild;
-        console.log(child);
-        while(child) {
-            container.removeChild(child);
-            child = container.lastElementChild;
-        };
-        console.log("====>")
-        console.log(container.lastElementChild);
-
-        
-    } 
-
-    let sizeSquare = setSquareSize(number);
+  
+function setGrid(number, sizeSquare) {
+    console.log("function setGrid(number)");
+    console.log("number: " + number)
+    console.log("sizeSquare: " + sizeSquare);
+    //let sizeSquare = setSquareSize(number);
     //console.log(sizeSquare);
     for(let i=0; i < number; i++) {
         // create one row
@@ -58,7 +29,36 @@ function setGrid(number) {
         // append row to container
         container.appendChild(row);
     }
-}
+};
+
+function setSquareSize(number) {
+    console.log("function setSquareSize()");
+    // calculate size of squares
+    // 1. get container width
+    let sizeContainer = document.querySelector("#container").clientWidth;
+    console.group(sizeContainer);
+    // 2.calculate square width/height
+    let size = (sizeContainer / number);
+    size = size +"px";
+    console.log(size)
+    return size;
+};
+
+function deleteOldGrid() {
+    console.log("function deleteOldGrid()");
+    // check if a grid is there, if so delete before new created
+    if (container.lastElementChild != null) {
+        let child = container.lastElementChild;
+        console.log(child);
+        while(child) {
+            container.removeChild(child);
+            child = container.lastElementChild;
+        };
+        console.log("====>")
+        console.log(container.lastElementChild);   
+    }
+};
+
 
 function getSquareCount() {
     console.log("function getSquareCount()");
@@ -68,19 +68,28 @@ function getSquareCount() {
     
     // check if user input is a number and not bigger than 100
     if (number === number && number <= 100) {
-        setGrid(number);
+        //setGrid(number);
+        return number;
     }
 }
 
 
+function main() {
+    console.log("function main()");
+    // 1. get user inut
+    let number = getSquareCount();
+    console.log(number);
+    // 2 check if a grid is there, if so delete old grid
+    deleteOldGrid();
+    // 3. calculate size of a square
+    let sizeSquare = setSquareSize(number);
+    // 3. create Grid
+    setGrid(number, sizeSquare);
+};
 
-
-let btnGrid = document.querySelector("#btnGrid").addEventListener("click", getSquareCount);
-//btnGrid.window.prompt("How many squares per side?")
-
+let btnGrid = document.querySelector("#btnGrid").addEventListener("click", main);
 const container = document.querySelector("#container");
- // create row outside of loop because namespace
-//setGrid();
+
 
 
 
