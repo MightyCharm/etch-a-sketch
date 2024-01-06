4277cb6d07747399cd591059d8f9da831360652c
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-01-06 08:21:19 +0100

    Add function createGrid() and hover()
    JavaScript now creates a 16x16 grid of square divs and if user
    hovers over one square, the background color switches to black.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+
+function createGrid() {
+
+    for(let i=0; i < 16; i++) {
+        console.log("loop 1 start");
+        // create one row
+        row = document.createElement("div");
+        row.setAttribute("class", "row");
+        for(let j=0; j < 16; j++) {
+            // create column
+            let column = document.createElement("div");
+            column.setAttribute("class", "column")
+            // add event listener to every column;
+            column.addEventListener("mouseover", hover)
+            // append column to row
+            row.appendChild(column);
+        }
+        // append row to container
+        container.appendChild(row);
+    }
+}
+
+function hover(e) {
+    console.log(e.target);
+    e.target.style.backgroundColor = "black";
+
+}
+
 const container = document.querySelector("#container");
+let row; // create row outside of loop because namespace
+createGrid();
+console.log(row);
+
+
