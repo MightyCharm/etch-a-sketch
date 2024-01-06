76a1751775e4f9c9ded679616f9e3bfd39ac7830
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-01-06 10:21:29 +0100

    Add button and feature that calculates square size

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
+function hover(e) {
+    console.log("function hover()");
+    //console.log(e.target);
+    e.target.style.backgroundColor = "black";
 
-function createGrid() {
+}
 
-    for(let i=0; i < 16; i++) {
-        console.log("loop 1 start");
+
+function setSquareSize(number) {
+    console.log("function setSquareSize()");
+
+    // calculate size of squares
+    // 1. get container width
+    let sizeContainer = document.querySelector("#container").clientWidth;
+    console.group(sizeContainer);
+    // 2.calculate square width/height
+    let size = (sizeContainer / number);
+    size = size +"px";
+    console.log(size)
+    return size;
+
+}
+
+
+function setGrid(number) {
+    console.log("function setGrid()");
+    //console.log(number);
+
+    // check if a grid is there, if so delete before new created
+
+
+    let sizeSquare = setSquareSize(number);
+    //console.log(sizeSquare);
+    for(let i=0; i < number; i++) {
         // create one row
-        row = document.createElement("div");
+        let row = document.createElement("div");
         row.setAttribute("class", "row");
-        for(let j=0; j < 16; j++) {
+        for(let j=0; j < number; j++) {
             // create column
             let column = document.createElement("div");
             column.setAttribute("class", "column")
+            column.style.width = sizeSquare;
+            column.style.height = sizeSquare;
             // add event listener to every column;
             column.addEventListener("mouseover", hover)
             // append column to row
@@ -20,15 +51,25 @@ function createGrid() {
     }
 }
 
-function hover(e) {
-    console.log(e.target);
-    e.target.style.backgroundColor = "black";
-
+function getSquareCount() {
+    console.log("function getSquareCount()");
+    // get user input
+    let number = +(window.prompt("How many squares in row and column?"));
+    //console.log(number);
+    
+    // check if user input is a number and not bigger than 100
+    if (number === number && number <= 100) {
+        setGrid(number);
+    }
 }
 
+
+let btnGrid = document.querySelector("#btnGrid").addEventListener("click", getSquareCount);
+//btnGrid.window.prompt("How many squares per side?")
+
 const container = document.querySelector("#container");
-let row; // create row outside of loop because namespace
-createGrid();
-console.log(row);
+ // create row outside of loop because namespace
+//setGrid();
+
 
 
